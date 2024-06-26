10886e4000a04db5c5d54e0ec4f2fc8e0f61e037
ibs-apple <ilyosbeksalayev240@gmail.com>
2024-06-26 21:37:37 +0500

    idea

---
+++ components/header/index.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header >
+        <div className="container">
+            <div className='flex items-center'>
+              <div className="flex">
+                <Link href='#'><link rel="stylesheet" href="" />aksia va chegirmalar</Link>
+              </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
