d985327b103c7624416db74a5188ebf3dc04d2c1
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-06-28 14:11:26 +0500

    idea

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Header from "~/components/header";
 
 export default function Home() {
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
+import IconMap from '~/icon'
 
 const Header = () => {
   return (
     <header >
         <div className="container">
             <div className='flex items-center'>
-              <div className="flex">
-                <Link href='#'><link rel="stylesheet" href="" />aksia va chegirmalar</Link>
+              <div className="flex gap-4">
+                <Link href='#'><span className='text-[36px] text-pink-500 font-bold '>%</span> aksia va chegirmalar</Link>
+                <Link href='#' className='flex items-center'><IconMap /> Dokon</Link>
               </div>
             </div>
         </div>
