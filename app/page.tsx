5fc9998a9e9d6812169bb9cff85a15916251f857
adfs <89073043+amiryusupov@users.noreply.github.com>
2024-08-21 19:08:20 +0500

    Idea

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BannerNavBar from "~/components/banner-nav";
 import Header from "~/components/header";
 
 export default function Home() {
   return (
     <div className="h-full">
       <Header />
+      <BannerNavBar />
     </div>
   );
 }
--- components/banner-nav/index.tsx
+++ components/banner-nav/index.tsx
@@ -1,31 +1,30 @@
-import Link from 'next/link'
-import React from 'react'
-import LogoIcon from '~/icon/logo'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu"
+"use client";
+import Link from "next/link";
+import React from "react";
+import LogoIcon from "~/icon/logo";
 
 const BannerNavBar = () => {
   return (
-    <div className=' container'>
+    <div className="container flex items-center gap-[4rem] mt-5">
       <div>
-        <Link href='/' ><LogoIcon /></Link>
+        <Link href="/" className="m-0">
+          <LogoIcon />
+        </Link>
       </div>
-      <nav></nav>
-
+      <nav className="select">
+        <select name="format" id="format">
+          <option selected disabled>
+            
+          </option>
+          <option value="pdf">PDF</option>
+          <option value="txt">txt</option>
+          <option value="epub">ePub</option>
+          <option value="fb2">fb2</option>
+          <option value="mobi">mobi</option>
+        </select>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default BannerNavBar
+export default BannerNavBar;
